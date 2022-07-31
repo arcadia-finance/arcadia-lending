85eaf05563c85555658b009ca80e9242ab43192d
Thomas Smets <thomass-smets@hotmail.com>
2022-07-31 13:41:50 +0200

    ✨ Implement deposit/withdrawal logic

--- src/LiquidityPool.sol
+++ src/LiquidityPool.sol
@@ -2,23 +2,33 @@
 pragma solidity ^0.8.13;
 
 import "../lib/solmate/src/auth/Owned.sol";
-//import "../lib/solmate/src/tokens/ERC20.sol";
 import "../lib/solmate/src/mixins/ERC4626.sol";
+import {SafeTransferLib} from "../lib/solmate/src/utils/SafeTransferLib.sol";
+
 
 contract LiquidityPool is ERC4626, Owned {
+    using SafeTransferLib for ERC20;
 
-    address[] tranches;
-    uint256[] weights;
     uint256 totalWeight;
+    address liquidator;
+
+    uint256[] weights;
+    address[] tranches;
+
+    mapping(address => bool) isTranche;
+
+    modifier onlyTranche() {
+        require(isTranche[msg.sender], "UNAUTHORIZED");
+        _;
+    }
 
     constructor(
         ERC20 _asset,
         string memory _name,
-        string memory _symbol
-    ) ERC4626(_asset, _name, _symbol) Owned(msg.sender) {}
-
-    function deposit(uint256 assets, address receiver) public override returns (uint256 shares) {
-        return super.deposit(assets, receiver);
+        string memory _symbol,
+        address _liquidator
+    ) ERC4626(_asset, _name, _symbol) Owned(msg.sender) {
+        liquidator = _liquidator;
     }
 
     //For now manually add newly created tranche, do via factory in future?
@@ -26,10 +36,105 @@ contract LiquidityPool is ERC4626, Owned {
         totalWeight += weight;
         weights.push(weight);
         tranches.push(tranche);
+        isTranche[tranche] = true;
+        asset.approve(tranche, type(uint256).max);
     }
 
     function setWeight(uint256 index, uint256 weight) public onlyOwner {
         weights[index] = weight;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        DEPOSIT/WITHDRAWAL LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function deposit(uint256 assets, address receiver) public override returns (uint256 shares) {
+        require(isTranche[receiver], "NO_TRANCHE");
+        // Check for rounding error since we round down in previewDeposit.
+        require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
+
+        // Need to transfer before minting or ERC777s could reenter.
+        asset.safeTransferFrom(msg.sender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        afterDeposit(assets, shares);
+    }
+
+    function mint(uint256 shares, address receiver) public override returns (uint256 assets) {
+        require(isTranche[receiver], "NO_TRANCHE");
+        assets = previewMint(shares); // No need to check for rounding error, previewMint rounds up.
+
+        // Need to transfer before minting or ERC777s could reenter.
+        asset.safeTransferFrom(msg.sender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        afterDeposit(assets, shares);
+    }
+
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) public override returns (uint256 shares) {
+        shares = previewWithdraw(assets); // No need to check for rounding error, previewWithdraw rounds up.
+
+        if (msg.sender != owner) {
+            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max) allowance[owner][msg.sender] = allowed - shares;
+        }
+
+        beforeWithdraw(assets, shares);
+
+        _burn(owner, shares);
+
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+
+        asset.safeTransfer(receiver, assets);
+    }
+
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) public override returns (uint256 assets) {
+        if (msg.sender != owner) {
+            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max) allowance[owner][msg.sender] = allowed - shares;
+        }
+
+        // Check for rounding error since we round down in previewRedeem.
+        require((assets = previewRedeem(shares)) != 0, "ZERO_ASSETS");
+
+        beforeWithdraw(assets, shares);
+
+        _burn(owner, shares);
+
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+
+        asset.safeTransfer(receiver, assets);
+    }
+
+    function depositViaTranche(uint256 assets) external onlyTranche {
+        uint256 shares = previewDeposit(assets);
+        // Check for rounding error since we round down in previewDeposit.
+        require(shares != 0, "ZERO_SHARES");
+
+        _mint(msg.sender, shares);
+    }
+
+    function withdrawViaTranche(uint256 assets) external onlyTranche {
+        uint256 shares = previewWithdraw(assets); // No need to check for rounding error, previewWithdraw rounds up.
+
+        _burn(owner, shares);
+
     }
 
     /*//////////////////////////////////////////////////////////////
--- src/Tranche.sol
+++ src/Tranche.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.13;
 
 import "../lib/solmate/src/auth/Owned.sol";
-//import "../lib/solmate/src/tokens/ERC20.sol";
 import "../lib/solmate/src/mixins/ERC4626.sol";
+import {SafeTransferLib} from "../lib/solmate/src/utils/SafeTransferLib.sol";
+import "./interfaces/ILiquidityPool.sol";
 
 contract LiquidityPool is ERC4626, Owned {
+    using SafeTransferLib for ERC20;
 
     ERC4626 liquidityPool;
 
@@ -16,6 +18,89 @@ contract LiquidityPool is ERC4626, Owned {
         ERC4626 _liquidityPool
     ) ERC4626(_asset, _name, _symbol) Owned(msg.sender) {
         liquidityPool = _liquidityPool;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        DEPOSIT/WITHDRAWAL LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function deposit(uint256 assets, address receiver) public override returns (uint256 shares) {
+        // Check for rounding error since we round down in previewDeposit.
+        require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
+        ILiquidityPool(address(liquidityPool)).depositViaTranche(assets);
+
+        // Need to transfer before minting or ERC777s could reenter.
+        asset.safeTransferFrom(msg.sender, address(liquidityPool), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        //afterDeposit(assets, shares);
+
+    }
+
+    function mint(uint256 shares, address receiver) public override returns (uint256 assets) {
+        assets = previewMint(shares); // No need to check for rounding error, previewMint rounds up.
+        ILiquidityPool(address(liquidityPool)).depositViaTranche(assets);
+
+        // Need to transfer before minting or ERC777s could reenter.
+        asset.safeTransferFrom(msg.sender, address(liquidityPool), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        //afterDeposit(assets, shares);
+
+    }
+
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address _owner
+    ) public override returns (uint256 shares) {
+        shares = previewWithdraw(assets); // No need to check for rounding error, previewWithdraw rounds up.
+
+        if (msg.sender != _owner) {
+            uint256 allowed = allowance[_owner][msg.sender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max) allowance[_owner][msg.sender] = allowed - shares;
+        }
+
+        ILiquidityPool(address(liquidityPool)).withdrawViaTranche(assets);
+
+        //beforeWithdraw(assets, shares);
+
+        _burn(_owner, shares);
+
+        emit Withdraw(msg.sender, receiver, _owner, assets, shares);
+
+        asset.safeTransfer(receiver, assets);
+    }
+
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address _owner
+    ) public override returns (uint256 assets) {
+        if (msg.sender != _owner) {
+            uint256 allowed = allowance[_owner][msg.sender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max) allowance[_owner][msg.sender] = allowed - shares;
+        }
+
+        // Check for rounding error since we round down in previewRedeem.
+        require((assets = previewRedeem(shares)) != 0, "ZERO_ASSETS");
+        ILiquidityPool(address(liquidityPool)).withdrawViaTranche(assets);
+
+        //beforeWithdraw(assets, shares);
+
+        _burn(_owner, shares);
+
+        emit Withdraw(msg.sender, receiver, _owner, assets, shares);
+
+        asset.safeTransfer(receiver, assets);
     }
 
     /*//////////////////////////////////////////////////////////////
