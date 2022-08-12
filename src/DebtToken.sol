375b8bc97d5fae7ba3e250d99dff634080d8894c
Thomas Smets <thomass-smets@hotmail.com>
2022-08-12 17:00:22 +0200

    Add debt token according the ERC-4626 standard

---
+++ src/DebtToken.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "../lib/solmate/src/auth/Owned.sol";
+import "../lib/solmate/src/mixins/ERC4626.sol";
+import {SafeTransferLib} from "../lib/solmate/src/utils/SafeTransferLib.sol";
+import "./interfaces/ILiquidityPool.sol";
+
+contract DebtToken is ERC4626, Owned {
+    using SafeTransferLib for ERC20;
+
+    constructor(
+        ERC20 _asset,
+        string memory _name,
+        string memory _symbol,
+        address _liquidityPool
+    ) ERC4626(_asset, _name, _symbol) Owned(msg.sender) {
+        liquidityPool = _liquidityPool;
+    }
+
+    address liquidityPool;
+
+    modifier onlyLiquidityPool() {
+        require(liquidityPool == msg.sender, "UNAUTHORIZED");
+        _;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            DEBT LOGIC
+    //////////////////////////////////////////////////////////////*/
+    uint256 public totalDebt;
+
+    function totalAssets() public view override returns (uint256) {
+        return totalDebt;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        DEPOSIT/WITHDRAWAL LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function deposit(uint256 assets, address receiver) public override onlyLiquidityPool returns (uint256 shares) {
+        // Check for rounding error since we round down in previewDeposit.
+        require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
+
+        _mint(receiver, shares);
+
+        totalDebt += assets;
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        //afterDeposit(assets, shares);
+
+    }
+
+    function mint(uint256, address) public pure override returns (uint256) {
+        revert('MINT_NOT_SUPPORTED');
+    }
+
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address _owner
+    ) public override onlyLiquidityPool returns (uint256 shares) {
+        shares = previewWithdraw(assets); // No need to check for rounding error, previewWithdraw rounds up.
+
+        _burn(_owner, shares);
+
+        totalDebt -= assets;
+
+        emit Withdraw(msg.sender, receiver, _owner, assets, shares);
+
+    }
+
+    function redeem(uint256, address, address) public pure override returns (uint256) {
+        revert('REDEEM_NOT_SUPPORTED');
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            INTERESTS LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function syncInterests(uint256 assets) public onlyLiquidityPool {
+        totalDebt += assets;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            TRANSFER LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function approve(address, uint256) public pure override returns (bool) {
+        revert('APPROVE_NOT_SUPPORTED');
+    }
+
+    function transfer(address, uint256) public pure override returns (bool) {
+        revert('TRANSFER_NOT_SUPPORTED');
+    }
+
+    function transferFrom(address, address, uint256) public pure override returns (bool) {
+        revert('TRANSFERFROM_NOT_SUPPORTED');
+    }
+
+    function permit(
+        address,
+        address,
+        uint256,
+        uint256,
+        uint8,
+        bytes32,
+        bytes32
+    ) public pure override {
+        revert('PERMIT_NOT_SUPPORTED');
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                          INTERNAL HOOKS LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function beforeWithdraw(uint256 assets, uint256 shares) internal override {}
+
+    function afterDeposit(uint256 assets, uint256 shares) internal override {}
+}
