bb51eaedd680524e1d959eaf6e994e76cbc5ac47
Thomas Smets <thomass-smets@hotmail.com>
2022-08-15 10:41:56 +0200

    ✅ Rename testfile

--- test/LiquidityPool.t.sol
+++ test/LiquidityPool.t.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "../lib/forge-std/src/Test.sol";
 
-contract ContractTest is Test {
+contract LiquidityPoolTest is Test {
     function setUp() public {}
 
     function testExample() public {
