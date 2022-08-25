8591bdbd751b7f77a05433cfdb0cebf3f152c95d
Thomas Smets <thomass-smets@hotmail.com>
2022-08-25 13:21:37 +0200

    ✅ Add tests for deposit/withdraw logic

--- src/Tranche.sol
+++ src/Tranche.sol
@@ -10,6 +10,12 @@ import "../lib/solmate/src/auth/Owned.sol";
 import "../lib/solmate/src/mixins/ERC4626.sol";
 import {SafeTransferLib} from "../lib/solmate/src/utils/SafeTransferLib.sol";
 
+/**
+ * @title tranche
+ * @author Arcadia Finance
+ * @notice The Logic to provide Liquidity for a lending pool for a certain ERC20 token
+ * @dev Protocol is according the ERC4626 standard, with a certain ERC20 as underlying
+ */
 contract Tranche is ERC4626, Owned {
     using SafeTransferLib for ERC20;
 
@@ -17,10 +23,17 @@ contract Tranche is ERC4626, Owned {
     bool public locked = false;
 
     modifier notLocked() {
-        require(!locked, "LOCKED");
+        require(!locked, "TRANCHE: LOCKED");
         _;
     }
 
+    /**
+     * @notice The constructor for a tranche
+     * @param _liquidityPool the Liquidity Pool of the underlying ERC-20 token, with the lending logic.
+     * @param _prefix The prefix of the contract name (eg. Senior -> Mezzanine -> Junior)
+     * @param _prefixSymbol The prefix of the contract symbol (eg. SR  -> MZ -> JR)
+     * @dev The name and symbol of the tranche are automatically generated, based on the name and symbol of the underlying token
+     */
     constructor(
         ERC4626 _liquidityPool,
         string memory _prefix,
@@ -37,11 +50,20 @@ contract Tranche is ERC4626, Owned {
                         LOCKING LOGIC
     //////////////////////////////////////////////////////////////*/
 
+    /**
+     * @notice Locks the tranche in case all liquidity of the tranche is written of due to bad debt
+     * @dev Only the Liquidity Pool can call this function, only trigger is a severe default event.
+     */
     function lock() public {
         require(msg.sender == address(liquidityPool), "UNAUTHORIZED");
         locked = true;
     }
 
+    /**
+     * @notice Unlocks the tranche.
+     * @dev Only the Owner can call this function, since tranches are locked due to complete defaults,
+     *      This function will only be called to partially refund existing share-holders after a default.
+     */
     function unLock() public onlyOwner {
         locked = false;
     }
@@ -50,6 +72,15 @@ contract Tranche is ERC4626, Owned {
                         DEPOSIT/WITHDRAWAL LOGIC
     //////////////////////////////////////////////////////////////*/
 
+    /**
+     * @notice Modification of the standard ERC-4626 deposit implementation
+     * @param assets tTe amount of assets of the underlying ERC-20 token being deposited
+     * @param receiver The address that receives the minted shares.
+     * @return shares The amount of shares minted
+     * @dev This contract does not directly transfers the underlying assets from the sender to the receiver.
+     *      Instead it calls the deposit of the Liquidity Pool which calls the transferFrom of the underlying assets.
+     *      Hence the sender should not give this contract an allowance to transfer the underlying asset but the Liquidity Pool.
+     */
     function deposit(uint256 assets, address receiver) public override notLocked returns (uint256 shares) {
         // Check for rounding error since we round down in previewDeposit.
         require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
@@ -62,6 +93,15 @@ contract Tranche is ERC4626, Owned {
         emit Deposit(msg.sender, receiver, assets, shares);
     }
 
+    /**
+     * @notice Modification of the standard ERC-4626 mint implementation
+     * @param shares The amount of shares minted
+     * @param receiver The address that receives the minted shares.
+     * @return assets The corresponding amount of assets of the underlying ERC-20 token being deposited
+     * @dev This contract does not directly transfers the underlying assets from the sender to the receiver.
+     *      Instead it calls the deposit of the Liquidity Pool which calls the transferFrom of the underlying assets.
+     *      Hence the sender should not give this contract an allowance to transfer the underlying asset but the Liquidity Pool.
+     */
     function mint(uint256 shares, address receiver) public override notLocked returns (uint256 assets) {
         assets = previewMint(shares); // No need to check for rounding error, previewMint rounds up.
 
@@ -73,43 +113,57 @@ contract Tranche is ERC4626, Owned {
         emit Deposit(msg.sender, receiver, assets, shares);
     }
 
+    /**
+     * @notice Modification of the standard ERC-4626 withdraw implementation
+     * @param assets The amount of assets of the underlying ERC-20 token being withdrawn
+     * @param receiver The address of the receiver of the underlying ERC-20 tokens
+     * @param owner_ The address of the owner of the assets being withdrawn
+     * @return shares The corresponding amount of shares redeemed
+     */
     function withdraw(
         uint256 assets,
         address receiver,
-        address _owner
+        address owner_
     ) public override notLocked returns (uint256 shares) {
         shares = previewWithdraw(assets); // No need to check for rounding error, previewWithdraw rounds up.
 
-        if (msg.sender != _owner) {
-            uint256 allowed = allowance[_owner][msg.sender]; // Saves gas for limited approvals.
+        if (msg.sender != owner_) {
+            uint256 allowed = allowance[owner_][msg.sender]; // Saves gas for limited approvals.
 
-            if (allowed != type(uint256).max) allowance[_owner][msg.sender] = allowed - shares;
+            if (allowed != type(uint256).max) allowance[owner_][msg.sender] = allowed - shares;
         }
 
         liquidityPool.withdraw(assets, receiver, address(this));
 
-        _burn(_owner, shares);
+        _burn(owner_, shares);
 
-        emit Withdraw(msg.sender, receiver, _owner, assets, shares);
+        emit Withdraw(msg.sender, receiver, owner_, assets, shares);
     }
 
+    /**
+     * @notice Modification of the standard ERC-4626 redeem implementation
+     * @param shares the amount of shares being redeemed
+     * @param receiver The address of the receiver of the underlying ERC-20 tokens
+     * @param owner_ The address of the owner of the shares being redeemed
+     * @return assets The corresponding amount of assets withdrawn
+     */
     function redeem(
         uint256 shares,
         address receiver,
-        address _owner
+        address owner_
     ) public override notLocked returns (uint256 assets) {
-        if (msg.sender != _owner) {
-            uint256 allowed = allowance[_owner][msg.sender]; // Saves gas for limited approvals.
+        if (msg.sender != owner_) {
+            uint256 allowed = allowance[owner_][msg.sender]; // Saves gas for limited approvals.
 
-            if (allowed != type(uint256).max) allowance[_owner][msg.sender] = allowed - shares;
+            if (allowed != type(uint256).max) allowance[owner_][msg.sender] = allowed - shares;
         }
 
         // Check for rounding error since we round down in previewRedeem.
         require((assets = previewRedeem(shares)) != 0, "ZERO_ASSETS");
 
-        _burn(_owner, shares);
+        _burn(owner_, shares);
 
-        emit Withdraw(msg.sender, receiver, _owner, assets, shares);
+        emit Withdraw(msg.sender, receiver, owner_, assets, shares);
 
         liquidityPool.withdraw(assets, receiver, address(this));
     }
@@ -118,6 +172,11 @@ contract Tranche is ERC4626, Owned {
                             ACCOUNTING LOGIC
     //////////////////////////////////////////////////////////////*/
 
+    /**
+     * @notice Returns the total amount of underlying assets, to which liquidity providers have a claim
+     * @return assets The total amount of underlying assets, to which liquidity providers have a claim
+     * @dev The Liquidity Pool does the accounting of the outstanding claim on liquidity per tranche.
+     */
     function totalAssets() public view override returns (uint256 assets) {
         assets =  liquidityPool.maxWithdraw(address(this));
     }
--- test/Tranche.t.sol
+++ test/Tranche.t.sol
@@ -48,6 +48,9 @@ abstract contract TrancheTest is Test {
         tranche = new Tranche(pool, "Senior", "SR");
         pool.addTranche(address(tranche), 50);
         vm.stopPrank();
+
+        vm.prank(liquidityProvider);
+        asset.approve(address(pool), type(uint256).max);
     }
 }
 
@@ -129,40 +132,302 @@ contract DepositAndWithdrawalTest is TrancheTest {
     }
 
     //deposit
-    function testRevert_DepositLocked() public {}
+    function testRevert_DepositLocked(uint128 assets, address receiver) public {
+        vm.prank(address(pool));
+        tranche.lock();
 
-    function testRevert_DepositZeroShares() public {}
+        vm.startPrank(liquidityProvider);
+        vm.expectRevert("TRANCHE: LOCKED");
+        tranche.deposit(assets, receiver);
+        vm.stopPrank();
+    }
 
-    function testSuccess_Deposit() public {}
+    function testRevert_DepositZeroShares(address receiver) public {
+        vm.startPrank(liquidityProvider);
+        vm.expectRevert("ZERO_SHARES");
+        tranche.deposit(0, receiver);
+        vm.stopPrank();
+    }
+
+    function testSuccess_Deposit(uint128 assets, address receiver) public {
+        vm.assume(assets > 0);
+
+        vm.prank(liquidityProvider);
+        tranche.deposit(assets, receiver);
+
+        assertEq(tranche.maxWithdraw(receiver), assets);
+        assertEq(tranche.maxRedeem(receiver), assets);
+        assertEq(tranche.totalAssets(), assets);
+        assertEq(asset.balanceOf(address(pool)), assets);
+    }
 
     //mint
-    function testRevert_MintLocked() public {}
+    function testRevert_MintLocked(uint128 shares, address receiver) public {
+        vm.prank(address(pool));
+        tranche.lock();
 
-    function testSuccess_Mint() public {}
+        vm.startPrank(liquidityProvider);
+        vm.expectRevert("TRANCHE: LOCKED");
+        tranche.mint(shares, receiver);
+        vm.stopPrank();
+    }
+
+    function testSuccess_Mint(uint128 shares, address receiver) public {
+        vm.assume(shares > 0);
+
+        vm.prank(liquidityProvider);
+        tranche.mint(shares, receiver);
+
+        assertEq(tranche.maxWithdraw(receiver), shares);
+        assertEq(tranche.maxRedeem(receiver), shares);
+        assertEq(tranche.totalAssets(), shares);
+        assertEq(asset.balanceOf(address(pool)), shares);
+    }
 
     //withdraw
-    function testRevert_WithdrawLocked() public {}
+    function testRevert_WithdrawLocked(uint128 assets, address receiver, address owner) public {
+        vm.prank(address(pool));
+        tranche.lock();
 
-    function testRevert_WithdrawUnauthorised() public {}
+        vm.startPrank(liquidityProvider);
+        vm.expectRevert("TRANCHE: LOCKED");
+        tranche.withdraw(assets, receiver, owner);
+        vm.stopPrank();
+    }
 
-    function testRevert_WithdrawInsufficientApproval() public {}
+    function testRevert_WithdrawUnauthorised(uint128 assets, address receiver, address owner, address unprivilegedAddress) public {
+        vm.assume(unprivilegedAddress != owner);
+        vm.assume(assets > 0);
 
-    function testSuccess_WithdrawByOwner() public {}
+        vm.prank(liquidityProvider);
+        tranche.deposit(assets, owner);
 
-    function testSuccess_WithdrawByLimitedAuthorisedAddress() public {}
+        vm.startPrank(unprivilegedAddress);
+        vm.expectRevert(stdError.arithmeticError);
+        tranche.withdraw(assets, receiver, owner);
+        vm.stopPrank();
+    }
 
-    function testSuccess_WithdrawByMaxAuthorisedAddress() public {}
+    function testRevert_WithdrawInsufficientApproval(uint128 assetsDeposited, uint128 sharesAllowed, address receiver, address owner, address beneficiary) public {
+        vm.assume(beneficiary != owner);
+        vm.assume(assetsDeposited > 0);
+        vm.assume(assetsDeposited < sharesAllowed);
+
+        vm.prank(liquidityProvider);
+        tranche.deposit(assetsDeposited, owner);
+
+        vm.prank(owner);
+        tranche.approve(beneficiary, sharesAllowed);
+
+        vm.startPrank(beneficiary);
+        vm.expectRevert(stdError.arithmeticError);
+        tranche.withdraw(sharesAllowed, receiver, owner);
+        vm.stopPrank();
+    }
+
+    function testRevert_WithdrawInsufficientAssets(uint128 assetsDeposited, uint128 assetsWithdrawn, address owner, address receiver) public {
+        vm.assume(assetsDeposited > 0);
+        vm.assume(assetsDeposited < assetsWithdrawn);
+
+        vm.prank(liquidityProvider);
+        tranche.deposit(assetsDeposited, owner);
+
+        vm.startPrank(owner);
+        vm.expectRevert(stdError.arithmeticError);
+        tranche.withdraw(assetsWithdrawn, receiver, owner);
+        vm.stopPrank();
+    }
+
+    function testSuccess_WithdrawByOwner(uint128 assetsDeposited, uint128 assetsWithdrawn, address owner, address receiver) public {
+        vm.assume(assetsDeposited > 0);
+        vm.assume(assetsDeposited >= assetsWithdrawn);
+        vm.assume(receiver != liquidityProvider);
+        vm.assume(receiver != address(pool));
+
+        vm.prank(liquidityProvider);
+        tranche.deposit(assetsDeposited, owner);
+
+        vm.prank(owner);
+        tranche.withdraw(assetsWithdrawn, receiver, owner);
+
+        assertEq(tranche.maxWithdraw(owner), assetsDeposited - assetsWithdrawn);
+        assertEq(tranche.maxRedeem(owner), assetsDeposited - assetsWithdrawn);
+        assertEq(tranche.totalAssets(), assetsDeposited - assetsWithdrawn);
+        assertEq(asset.balanceOf(address(pool)), assetsDeposited - assetsWithdrawn);
+        assertEq(asset.balanceOf(receiver), assetsWithdrawn);
+    }
+
+    function testSuccess_WithdrawByLimitedAuthorisedAddress(uint128 assetsDeposited, uint128 sharesAllowed, uint128 assetsWithdrawn, address receiver, address owner, address beneficiary) public {
+        vm.assume(assetsDeposited > 0);
+        vm.assume(assetsDeposited >= assetsWithdrawn);
+        vm.assume(sharesAllowed >= assetsWithdrawn);
+        vm.assume(receiver != liquidityProvider);
+        vm.assume(receiver != address(pool));
+        vm.assume(beneficiary != owner);
+
+        vm.prank(liquidityProvider);
+        tranche.deposit(assetsDeposited, owner);
+
+        vm.prank(owner);        
+        tranche.approve(beneficiary, sharesAllowed);
+
+        vm.startPrank(beneficiary);
+        tranche.withdraw(assetsWithdrawn, receiver, owner);
+
+        assertEq(tranche.maxWithdraw(owner), assetsDeposited - assetsWithdrawn);
+        assertEq(tranche.maxRedeem(owner), assetsDeposited - assetsWithdrawn);
+        assertEq(tranche.totalAssets(), assetsDeposited - assetsWithdrawn);
+        assertEq(tranche.allowance(owner, beneficiary), sharesAllowed - assetsWithdrawn);
+        assertEq(asset.balanceOf(address(pool)), assetsDeposited - assetsWithdrawn);
+        assertEq(asset.balanceOf(receiver), assetsWithdrawn);
+    }
+
+    function testSuccess_WithdrawByMaxAuthorisedAddress(uint128 assetsDeposited, uint128 assetsWithdrawn, address receiver, address owner, address beneficiary) public {
+        vm.assume(assetsDeposited > 0);
+        vm.assume(assetsDeposited >= assetsWithdrawn);
+        vm.assume(receiver != liquidityProvider);
+        vm.assume(receiver != address(pool));
+        vm.assume(beneficiary != owner);
+
+        vm.prank(liquidityProvider);
+        tranche.deposit(assetsDeposited, owner);
+
+        vm.prank(owner);
+        tranche.approve(beneficiary, type(uint256).max);
+
+        vm.startPrank(beneficiary);
+        tranche.withdraw(assetsWithdrawn, receiver, owner);
+
+        assertEq(tranche.maxWithdraw(owner), assetsDeposited - assetsWithdrawn);
+        assertEq(tranche.maxRedeem(owner), assetsDeposited - assetsWithdrawn);
+        assertEq(tranche.totalAssets(), assetsDeposited - assetsWithdrawn);
+        assertEq(tranche.allowance(owner, beneficiary), type(uint256).max);
+        assertEq(asset.balanceOf(address(pool)), assetsDeposited - assetsWithdrawn);
+        assertEq(asset.balanceOf(receiver), assetsWithdrawn);
+    }
 
     //redeem
-    function testRevert_RedeemLocked() public {}
+    function testRevert_RedeemLocked(uint128 shares, address receiver, address owner) public {
+    vm.prank(address(pool));
+    tranche.lock();
 
-    function testRevert_RedeemUnauthorised() public {}
+    vm.startPrank(liquidityProvider);
+    vm.expectRevert("TRANCHE: LOCKED");
+    tranche.redeem(shares, receiver, owner);
+    vm.stopPrank();
+    }
 
-    function testRevert_RedeemInsufficientApproval() public {}
+    function testRevert_RedeemUnauthorised(uint128 shares, address receiver, address owner, address unprivilegedAddress) public {
+        vm.assume(unprivilegedAddress != owner);
+        vm.assume(shares > 0);
 
-    function testSuccess_RedeemByOwner() public {}
+        vm.prank(liquidityProvider);
+        tranche.mint(shares, owner);
 
-    function testSuccess_RedeemByLimitedAuthorisedAddress() public {}
+        vm.startPrank(unprivilegedAddress);
+        vm.expectRevert(stdError.arithmeticError);
+        tranche.redeem(shares, receiver, owner);
+        vm.stopPrank();
+    }
 
-    function testSuccess_RedeemByMaxAuthorisedAddress() public {}
+    function testRevert_RedeemInsufficientApproval(uint128 sharesMinted, uint128 sharesAllowed, address receiver, address owner, address beneficiary) public {
+        vm.assume(beneficiary != owner);
+        vm.assume(sharesMinted > 0);
+        vm.assume(sharesMinted < sharesAllowed);
+
+        vm.prank(liquidityProvider);
+        tranche.mint(sharesMinted, owner);
+
+        vm.prank(owner);
+        tranche.approve(beneficiary, sharesAllowed);
+
+        vm.startPrank(beneficiary);
+        vm.expectRevert(stdError.arithmeticError);
+        tranche.redeem(sharesAllowed, receiver, owner);
+        vm.stopPrank();
+    }
+
+    function testRevert_RedeemInsufficientShares(uint128 sharesMinted, uint128 sharesRedeemed, address owner, address receiver) public {
+        vm.assume(sharesMinted > 0);
+        vm.assume(sharesMinted < sharesRedeemed);
+
+        vm.prank(liquidityProvider);
+        tranche.mint(sharesMinted, owner);
+
+        vm.startPrank(owner);
+        vm.expectRevert(stdError.arithmeticError);
+        tranche.redeem(sharesRedeemed, receiver, owner);
+        vm.stopPrank();
+    }
+
+    function testSuccess_RedeemByOwner(uint128 sharesMinted, uint128 sharesRedeemed, address owner, address receiver) public {
+        vm.assume(sharesMinted > 0);
+        vm.assume(sharesRedeemed > 0);
+        vm.assume(sharesMinted >= sharesRedeemed);
+        vm.assume(receiver != liquidityProvider);
+        vm.assume(receiver != address(pool));
+
+        vm.prank(liquidityProvider);
+        tranche.mint(sharesMinted, owner);
+
+        vm.prank(owner);
+        tranche.redeem(sharesRedeemed, receiver, owner);
+
+        assertEq(tranche.maxWithdraw(owner), sharesMinted - sharesRedeemed);
+        assertEq(tranche.maxRedeem(owner), sharesMinted - sharesRedeemed);
+        assertEq(tranche.totalAssets(), sharesMinted - sharesRedeemed);
+        assertEq(asset.balanceOf(address(pool)), sharesMinted - sharesRedeemed);
+        assertEq(asset.balanceOf(receiver), sharesRedeemed);
+    }
+
+    function testSuccess_RedeemByLimitedAuthorisedAddress(uint128 sharesMinted, uint128 sharesAllowed, uint128 sharesRedeemed, address receiver, address owner, address beneficiary) public {
+        vm.assume(sharesMinted > 0);
+        vm.assume(sharesRedeemed > 0);
+        vm.assume(sharesMinted >= sharesRedeemed);
+        vm.assume(sharesAllowed >= sharesRedeemed);
+        vm.assume(receiver != liquidityProvider);
+        vm.assume(receiver != address(pool));
+        vm.assume(beneficiary != owner);
+
+        vm.prank(liquidityProvider);
+        tranche.mint(sharesMinted, owner);
+
+        vm.prank(owner);        
+        tranche.approve(beneficiary, sharesAllowed);
+
+        vm.startPrank(beneficiary);
+        tranche.redeem(sharesRedeemed, receiver, owner);
+
+        assertEq(tranche.maxWithdraw(owner), sharesMinted - sharesRedeemed);
+        assertEq(tranche.maxRedeem(owner), sharesMinted - sharesRedeemed);
+        assertEq(tranche.totalAssets(), sharesMinted - sharesRedeemed);
+        assertEq(tranche.allowance(owner, beneficiary), sharesAllowed - sharesRedeemed);
+        assertEq(asset.balanceOf(address(pool)), sharesMinted - sharesRedeemed);
+        assertEq(asset.balanceOf(receiver), sharesRedeemed);
+    }
+
+    function testSuccess_RedeemByMaxAuthorisedAddress(uint128 sharesMinted, uint128 sharesRedeemed, address receiver, address owner, address beneficiary) public {
+        vm.assume(sharesMinted > 0);
+        vm.assume(sharesRedeemed > 0);
+        vm.assume(sharesMinted >= sharesRedeemed);
+        vm.assume(receiver != liquidityProvider);
+        vm.assume(receiver != address(pool));
+        vm.assume(beneficiary != owner);
+
+        vm.prank(liquidityProvider);
+        tranche.mint(sharesMinted, owner);
+
+        vm.prank(owner);
+        tranche.approve(beneficiary, type(uint256).max);
+
+        vm.startPrank(beneficiary);
+        tranche.redeem(sharesRedeemed, receiver, owner);
+
+        assertEq(tranche.maxWithdraw(owner), sharesMinted - sharesRedeemed);
+        assertEq(tranche.maxRedeem(owner), sharesMinted - sharesRedeemed);
+        assertEq(tranche.totalAssets(), sharesMinted - sharesRedeemed);
+        assertEq(tranche.allowance(owner, beneficiary), type(uint256).max);
+        assertEq(asset.balanceOf(address(pool)), sharesMinted - sharesRedeemed);
+        assertEq(asset.balanceOf(receiver), sharesRedeemed);
+    }
 }
