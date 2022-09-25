cd61e5fdcabcd45e5c6ac36a9e057eb52bc32b22
Thomas Smets <thomass-smets@hotmail.com>
2022-09-25 16:04:43 +0200

    ✅ Fix failing test

--- test/LendingPool.t.sol
+++ test/LendingPool.t.sol
@@ -697,10 +697,10 @@ contract LoanTest is LendingPoolTest {
         pool.borrow(amountLoaned, address(vault), vaultOwner);
 
         vm.startPrank(sender);
-        // When: sender approve and repay amountLoaned
         asset.approve(address(pool), type(uint256).max);
-        // Then: repay should revert with "TRANSFER_FAILED"
-        vm.expectRevert("TRANSFER_FAILED");
+        // When: sender repays amountLoaned which is more than his available funds
+        // Then: repay should revert with an ovcerflow
+        vm.expectRevert(stdError.arithmeticError);
         pool.repay(amountLoaned, address(vault));
         vm.stopPrank();
     }
