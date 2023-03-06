200efaf7f30f5531e40dc3b8b7357b84ed9eab49
Thomas Smets <thomass-smets@hotmail.com>
2023-03-06 16:32:30 +0100

    📝 Update documentation InterestRateModule

--- src/libraries/DataTypes.sol
+++ src/libraries/DataTypes.sol
@@ -7,6 +7,13 @@
 pragma solidity ^0.8.13;
 
 library DataTypes {
+    /**
+     * A struct with the set of interest rate configuration parameters
+     * - baseRatePerYear The interest rate when utilisation is 0
+     * - lowSlopePerYear The slope of the first curve, defined as the delta in interest rate for a delta in utilisation of 100%
+     * - highSlopePerYear The slope of the second curve, defined as the delta in interest rate for a delta in utilisation of 100%
+     * - utilisationThreshold the optimal utilisation, where we go from the flat first curve to the steeper second curve
+     */
     struct InterestRateConfiguration {
         uint72 baseRatePerYear; //18 decimals precision
         uint72 lowSlopePerYear; //18 decimals precision
